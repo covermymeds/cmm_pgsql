ca5223be23cb19f6227c351fb88b8afacd79bb7f
CJ Estel <cestel@covermymeds.com>
2015-05-07 13:31:43 -0400

    added wrapper for pg_ident

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -25,6 +25,13 @@ class cmm_pgsql::setup (
     #include commvault backups
     include ::cmm_pgsql::commvault_backup
 
+    #setup pg_ident if values exist
+    $_pg_ident = $::cmm_pgsql::pg_ident 
+    unless empty($_pg_ident) {
+      create_resources(postgresql::server::pg_ident_rule, $_pg_ident)
+    }
+
+
     #setup postgres ssh keys
     unless empty($::cmm_pgsql::keysource) {
       file { "/var/lib/pgsql/.ssh":
