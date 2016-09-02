3b0f5325606acf4d96212352b154767b3bc73f61
Doug Morris <dmorris@covermymeds.com>
2016-09-02 07:54:57 -0400

    remove space, lint

--- manifests/monitoring_wrapper.pp
+++ manifests/monitoring_wrapper.pp
@@ -17,7 +17,7 @@ define cmm_pgsql::monitoring_wrapper (
   ::monitoring::target::service {"${monitor_description}-${::hostname}":
     command   => "${monitor_command}! $monitor_args",
     notes_url => $notes_url,
-    contacts => $monitor_contacts,
+    contacts  => $monitor_contacts,
   }
 
 }
